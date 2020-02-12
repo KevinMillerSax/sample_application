185cd3841eaa7706b4f66f76f41f2b170bd74bcd
Kevin Miller <KevinMiller@kevins-mbp.home>
2020-02-11 21:21:07 -0800

    User modeling

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validation tests' do
+    it 'validates name presence' do
+      user = User.new(name: "", email: "user@example.com").save
+      expect(user).to eq(false)
+    end
+
+    it "validates email presence" do
+      user = User.new(name: "Test User", email: "").save
+      expect(user).to eq(false)
+    end
+
+    it "validates name length is greater than 50" do
+      user = User.new(name: ("a"*51), email:"user@example.com").save
+      expect(user).to eq(false)
+    end
+
+    it "validates email length is greater than 255" do
+      user = User.new(name: "user", email: ("a" * 256)).save
+      expect(user).to eq(false)
+    end
+
+    it "validates email has correct format" do
+      user = User.new(name: "user", email: "kevinmiller.com").save
+      expect(user).to eq(false)
+    end
+
+    it "should have a unique email address" do
+      user = User.new(name: "user", email: "user@example.com").save
+      user2 = User.new(name: "user2", email: "user@example.com").save
+      expect(user2.email).not_to eq(user.email)
+    end
+
+    it "password should have a minimum length" do
+      user = User.new(name: "user", email: "user@example.com", password: "passw").save
+      expect(user).to eq(false)
+    end
+  end
+
+  context 'scope tests' do
+  end
+
+end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,26 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    get :home
+    get root_url
     assert_response :success
     assert_select "title",  "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get :help
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get :about
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
-    get :contact
-    assert_respose :success
+    get contact_path
+    assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
