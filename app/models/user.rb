b0ee4539b1006351b9db2afdedb84793454dec51
Kevin Miller <KevinMiller@kevins-mbp.home>
2020-02-15 18:00:07 -0800

    login work

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: {minimum: 6}
+
+  # return a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # remembers a user in the database 
+  def remember
+    self.remember_token = User.new_token
+  end
 end
